51f0756a417e5e96824ea7182482a4141f59879d
burakkagann <burak.kagan@protonmail.com>
2025-05-21 19:48:56 +0200

    yolov8n phase 1 setup config

--- src/scripts/visdrone/run_yolov8n_training.ps1
+++ src/scripts/visdrone/run_yolov8n_training.ps1
@@ -20,8 +20,9 @@ function Clean-Cache {
 # Step 3: Function to train YOLOv8
 function Train-YOLOv8 {
     Write-Host "Starting YOLOv8n training..."
-    python ..\..\..\src\models\YOLOv8\train.py --img 640 --batch 16 --epochs 5 --data ..\config\my_dataset.yaml --weights yolov8n.yaml --name yolo8n_baseline
+    yolo train model=yolov8n.pt data="C:\Users\burak\OneDrive\Desktop\Git Repos\drone-obj-detection-lightweight-ai\config\my_dataset.yaml" epochs=5 batch=16 imgsz=640 name=yolov8n_baseline
 }
+
 
 # Main script execution
 function Run-Training {
